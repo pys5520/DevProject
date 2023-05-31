eb0db805b458746238dab60a6ab96120d007702d
pys5520 <goodsoo5520@gmail.com>
2023-05-31 17:06:48 +0900

    보충

--- DevProject/src/main/java/kr/or/ddit/vo/Address.java
+++ DevProject/src/main/java/kr/or/ddit/vo/Address.java
@@ -1,10 +1,16 @@
 package kr.or.ddit.vo;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class Address {
+	// 문자열이 null이 아니고 trim한 길이가 0보다 크다는것을 검사한다.
+	@NotBlank
 	private String postCode;
+	// 문자열이 null이 아니고 trim한 길이가 0보다 크다는것을 검사한다.
+	@NotBlank
 	private String location;
 	
 }
--- DevProject/src/main/java/kr/or/ddit/vo/Member.java
+++ DevProject/src/main/java/kr/or/ddit/vo/Member.java
@@ -5,20 +5,36 @@ package kr.or.ddit.vo;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
 @Data
 public class Member {
+	@NotBlank(message = "야 ID가 비었잖아 --")
 	private String userId = "a001";
+	@NotBlank(message = "야 이름이 비었잖아 --")
+	@Size(max = 3)
 	private String userName = "hongkd";
+	@NotBlank(message = "야 비밀번호가 비었잖아 --")
 	private String password = "1234";
 	private int coin = 100;
-	@DateTimeFormat(pattern = "yyyyMMdd")
+	@Past
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date dateOfBirth;
+	// 중첩된 자바빈즈의 입력값 검증을 지정한다.
+	@Valid
 	private Address address;
+	// 중첩된 자바빈즈의 입력값 검증을 지정한다.
+	@Valid
 	private List<Card> cardList;
+	@Email
 	private String email;
 	private String gender;
 	private String hobby;
