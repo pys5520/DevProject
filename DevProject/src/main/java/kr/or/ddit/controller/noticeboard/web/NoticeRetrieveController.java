dc5e4a4e08f980f13ece4d48d6e0892fbe13fabd
pys5520 <goodsoo5520@gmail.com>
2023-06-02 12:15:49 +0900

    06/02 -2

---
+++ DevProject/src/main/java/kr/or/ddit/controller/noticeboard/web/NoticeRetrieveController.java
@@ -0,0 +1,18 @@
+package kr.or.ddit.controller.noticeboard.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/notice")
+@Slf4j
+public class NoticeRetrieveController {
+	
+	@RequestMapping(value = "/list.do")
+	public String noticeList() {
+		log.info("noticeList() 실행...!");
+		return "notice/list";
+	}
+}
